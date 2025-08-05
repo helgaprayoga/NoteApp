d8853e58d177405c9d3d30842a3e6ed570437c92
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-06 00:44:56 +0700

    feat: Add note creation screen

--- app/src/main/java/com/learn/noteapp/MainActivity.kt
+++ app/src/main/java/com/learn/noteapp/MainActivity.kt
@@ -19,29 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NoteAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NoteAppTheme {
-        Greeting("Android")
+
     }
 }
