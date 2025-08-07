a9bc4752699d63b9041b1be23d043cf4c07bcac4
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-07 22:51:47 +0700

    feat: Add note creation UI and logic

--- app/src/main/java/com/learn/noteapp/MainActivity.kt
+++ app/src/main/java/com/learn/noteapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.learn.noteapp.screen.NoteScreen
 import com.learn.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NoteAppTheme {
-
+                NoteScreen()
             }
         }
     }
--- app/src/main/java/com/learn/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/learn/noteapp/screen/NoteScreen.kt
@@ -12,27 +12,41 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.learn.noteapp.R
+import com.learn.noteapp.components.NoteButton
 import com.learn.noteapp.components.NoteInputText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteScreen() {
+    var title by remember {
+        mutableStateOf("")
+    }
+    var description by remember {
+        mutableStateOf("")
+    }
+    
     Column(
         modifier = Modifier.padding(6.dp)
     ) {
         TopAppBar(
-            title = { Text(text = stringResource(id = R.string.app_name)) },
-            actions = { Icon(
+            title = {
+                Text(text = stringResource(id = R.string.app_name)
+                )},
+            actions = {
+                Icon(
                     imageVector = Icons.Rounded.Notifications,
                     contentDescription = "Icon"
-            ) },
+                )},
             colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primaryContainer)
         )
 
@@ -41,7 +55,32 @@ fun NoteScreen() {
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NoteInputText(text = "Hello", label = "Hello", onTextChange = {})
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                        char.isLetter() || char.isWhitespace()
+                    }) title = it
+                }
+            )
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) description = it
+                }
+            )
+
+            NoteButton(
+                text = "Save",
+                onClick = {}
+            )
         }
     }
 }
