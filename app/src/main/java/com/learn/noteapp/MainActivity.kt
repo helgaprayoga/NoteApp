a1ff6baec0a8704ff259361721bcbbf58909cb92
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-07 23:53:03 +0700

    refactor: Introduce ViewModel for   state management

--- app/src/main/java/com/learn/noteapp/MainActivity.kt
+++ app/src/main/java/com/learn/noteapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,9 +14,11 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.learn.noteapp.data.NoteDataSource
 import com.learn.noteapp.model.Note
 import com.learn.noteapp.screen.NoteScreen
+import com.learn.noteapp.screen.NoteViewModel
 import com.learn.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,20 +27,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NoteAppTheme {
-                val notes = remember {
-                    mutableStateListOf<Note>()
-                }
-                NoteScreen(
-                    notes = notes,
-                    onAddNote = {
-                        notes.add(it)
-                    },
-                    onRemoveNote = {
-                        notes.remove(it)
-                    })
+                val noteViewModel: NoteViewModel by viewModels()
+                NotesApp(noteViewModel = noteViewModel)
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+
+    NoteScreen(
+        notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        }
+    )
 }
 
 @Preview(showBackground = true)
