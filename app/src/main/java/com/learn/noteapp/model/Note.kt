d9736a745f0b45635c011d15fd25bdbbaa89d020
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-11 23:15:04 +0700

    feat: Implement Room database for      local data persistence

--- app/src/main/java/com/learn/noteapp/model/Note.kt
+++ app/src/main/java/com/learn/noteapp/model/Note.kt
@@ -1,11 +1,22 @@
 package com.learn.noteapp.model
 
-import java.time.LocalDateTime
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalTime
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "note_entry_date")
+    val entryDate: LocalTime = LocalTime.now()
 )
